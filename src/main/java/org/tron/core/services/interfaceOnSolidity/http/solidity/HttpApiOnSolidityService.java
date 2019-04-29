2b820ca1b7dd53dd9c1e937f8db20a1db1d10047
Matt <yrp1990@gmail.com>
2019-04-29 11:47:46 +0800

    add some log

--- src/main/java/org/tron/core/services/interfaceOnSolidity/http/solidity/HttpApiOnSolidityService.java
+++ src/main/java/org/tron/core/services/interfaceOnSolidity/http/solidity/HttpApiOnSolidityService.java
@@ -90,7 +90,7 @@ public class HttpApiOnSolidityService implements Service {
       context.setContextPath("/");
       server.setHandler(context);
       for (Connector connector : server.getConnectors()) {
-        ((AbstractConnector) connector).setIdleTimeout(500);
+        ((AbstractConnector) connector).setIdleTimeout(1000);
       }
       // same as FullNode
       context.addServlet(new ServletHolder(accountOnSolidityServlet), "/walletsolidity/getaccount");
