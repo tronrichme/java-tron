ad15d59a31b9eeba82140ae83939eaf589955704
Matt <yrp1990@gmail.com>
2019-04-29 12:02:39 +0800

    add some log

--- src/main/java/org/tron/core/services/interfaceOnSolidity/http/solidity/HttpApiOnSolidityService.java
+++ src/main/java/org/tron/core/services/interfaceOnSolidity/http/solidity/HttpApiOnSolidityService.java
@@ -132,7 +132,7 @@ public class HttpApiOnSolidityService implements Service {
       context.addServlet(new ServletHolder(getNodeInfoOnSolidityServlet), "/wallet/getnodeinfo");
 
       server.start();
-      context.getSessionHandler().setMaxInactiveInterval(2);
+//      context.getSessionHandler().setMaxInactiveInterval(2);
     } catch (Exception e) {
       logger.debug("IOException: {}", e.getMessage());
     }
